7a9fa2241e74a513b6c683b3f0f1c3ca56dc7a21
Anton <rogulenkoaa@gmail.com>
2024-06-17 23:10:14 +0500

    add folder text (default)

--- src/main/java/com/javarush/rogulenko/cryptoanalizer/constants/Constants.java
+++ src/main/java/com/javarush/rogulenko/cryptoanalizer/constants/Constants.java
@@ -1,9 +1,12 @@
 package com.javarush.rogulenko.cryptoanalizer.constants;
 
+import java.io.File;
+
 public class Constants {
     private static final String rus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
     private static final String eng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String cypher = "0123456789";
     private static final String diff_sym = "!@#$%^&*()[]{}";
     public static final String ALPHABET = rus.toLowerCase() + eng.toLowerCase() + cypher + diff_sym;
+    public static final String TXT_FOLDER = System.getProperty("user.dir") + File.separator + "text" + File.separator;
 }
